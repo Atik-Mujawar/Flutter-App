38870fcc309b971ba25e0710c5d70e11aada70f6
Atik-Mujawar <atikmujawar92@gmail.com>
2022-02-23 20:59:28 +0530

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = 'Hello';
+
+    // double pi = 3.14;
+    // bool isTrue = true;
+    // num temp = 20.2; // can take both int and double
+
+    // var day = "Tuesday"; // automatically inferred type
+    // const day2 = "Monday"; // can't change value
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $days  days of  Flutter by $name'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
